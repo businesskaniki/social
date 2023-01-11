590a5c632ae2b4ccc7e8d6c83d38543484c63dc9
“businesskaniki” <businesskanikii@gmail.com>
2023-01-11 14:56:26 +0300

    still remaining with making the chats scroll alone

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
+import Chats from './chats/Chats'
 import Nav from './nav/Nav'
 
 function Layout() {
   return (
-    <div className='bg-gray-700'>
-       <div className="p-3">
+    <div className='bg-gray-700 h-full '>
+       <div className="p-3 border-r border-gray-700 w-full overflow-x-hidden">
          <Nav />
+         <Chats />
        </div>
     </div>
   )
---
+++ src/components/chats/Chats.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import Chat from './Chat';
+function Chats() {
+  return (
+    <div className='pt-5 pb-3 w-1/4 overflow-auto'>
+      <Chat />
+      <Chat />
+      <Chat />
+      <Chat />
+      <Chat />
+      <Chat />
+      <Chat />
+      <Chat />
+      <Chat />
+      <Chat />
+      <Chat />
+      <Chat />
+      <Chat />
+      <Chat />
+    </div>
+  );
+}
+
+export default Chats;
